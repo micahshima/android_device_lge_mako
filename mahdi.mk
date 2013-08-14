1615b935b77958da9b46bae03dbed18118210a75
Mahdi-Rom <mahdi.rom@gmail.com>
2013-08-14 01:01:35 +0100

    mahdi.mk: some minor changes

--- mahdi.mk
+++ mahdi.mk
@@ -1,30 +1,29 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/mahdi/config/gsm.mk)
+# Release name
+PRODUCT_RELEASE_NAME := Nexus4
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 768
-
-# Inherit common product files.
-$(call inherit-product, vendor/mahdi/config/common.mk)
-
-# Inherit some common stuff.
-$(call inherit-product, vendor/mahdi/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/mahdi/config/nfc_enhanced.mk)
-
-# Inherit device configuration
+# Inherit AOSP device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
-## Device identifier. This must come after all inclusions
+# Inherit some common stuff.
+$(call inherit-product, vendor/mahdi/configs/common.mk)
+$(call inherit-product, vendor/mahdi/configs/common_full_phone.mk)
+
+# Inherit GSM files.
+$(call inherit-product, vendor/mahdi/configs/gsm.mk)
+
+# Setup device specific product configuration.
 PRODUCT_DEVICE := mako
 PRODUCT_NAME := mahdi_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
+# Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.3/JWR66V/737497:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.3 JWR66V 737497 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/mahdi/prebuilt/bootanimations/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
