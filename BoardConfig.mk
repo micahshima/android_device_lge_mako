0bc07307d0252a506a0cbea6b33f223e0e368839
Mahdi-Rom <mahdi.rom@gmail.com>
2013-11-20 20:53:26 +0000

    BoardConfig.mk: remove prebuilt kernel
    add defconfig for my kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,12 +25,13 @@ TARGET_CPU_VARIANT := krait
 
 TARGET_NO_BOOTLOADER := true
 
-TARGET_PREBUILT_KERNEL := device/lge/mako-kernel/kernel
-
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
+
+# Try to build the kernel
+TARGET_KERNEL_CONFIG := mahdi-mako_defconfig
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
